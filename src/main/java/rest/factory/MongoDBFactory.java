61c10acd68aa38adcc7616ff32c49da96826f857
sasa <alex@razlet.ru>
2016-07-17 15:30:08 +0700

    Добавил работу с mongo

--- src/main/java/rest/factory/MongoDBFactory.java
+++ src/main/java/rest/factory/MongoDBFactory.java
@@ -1,19 +1,32 @@
 package rest.factory;
 
+import java.net.UnknownHostException;
+
+
 import org.glassfish.hk2.api.Factory;
 
 import com.mongodb.Mongo;
+import com.mongodb.MongoException;
 
 public class MongoDBFactory implements Factory<Mongo> {
 
-	private Mongo client;
+	
 	
 	public void dispose(Mongo arg0) {
-		client = arg0;
+		
 	}
 
 	public Mongo provide() {
-		return client;
+		
+		try {
+			return new Mongo("127.0.0.1",27017);
+		} catch (UnknownHostException e) {
+			System.out.println("Неизвестный хост "+e.getMessage());
+		} catch (MongoException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return null;
 	}
 
 }
--- src/main/java/rest/test/MongoRest.java
+++ src/main/java/rest/test/MongoRest.java
@@ -1,13 +1,24 @@
 package rest.test;
 
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
+import com.mongodb.ObjectId;
 
 @Path("/mongo")
 public class MongoRest {
@@ -19,11 +30,23 @@ public class MongoRest {
 	@Produces("application/json")
 	public String getHello() {
 		try {
-			System.out.println("mongo");
-			DB dataBase = mongo.getDB("rest");
-			System.out.println("???" + dataBase.getLastError());
-			DBObject result = dataBase.command("db.test.find()");
-			return result.toString();
+			DB dataBase = mongo.getDB("test");
+			
+			DBCollection result = dataBase.getCollection("test");
+			
+			StringBuffer sb = new StringBuffer();
+			
+			List<DBObject> _list = result.find().toArray();
+
+			if(_list.isEmpty()){
+				return "{\"result\":\"nothin was found\"}";
+			}
+			
+			for(int i=0;i<_list.size();i++){
+				sb.append(_list.get(i).toString());
+			}
+			
+			return sb.toString();
 		} catch (Exception e) {
 			System.out.println(e);
 			return "{\"error\":\""+e.getMessage()+"\"}";
@@ -31,4 +54,82 @@ public class MongoRest {
 		
 	}
 
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
+	public String insertData(String query)
+	{
+		DB dataBase = mongo.getDB("test");
+		DBCollection collection = dataBase.getCollection("test");
+		
+		DBObject doc = new BasicDBObject();
+		doc.put("name", "test1");
+		collection.insert(doc);
+		
+		DBCursor cursor = collection.find();
+		
+		return cursor.next().toString();
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public String getById(@PathParam("id") String id)
+	{
+		
+		
+		DB dataBase = mongo.getDB("test");
+		DBCollection collection = dataBase.getCollection("test");
+		
+		return collection.findOne(createObjectById(id)).toString();
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public String updateData(@PathParam("id") String id,String query)
+	{
+		
+		if(id==null){
+			return "{\"error\":\"id is absent\"}";
+		}
+		
+		DB dataBase = mongo.getDB("test");
+		DBCollection collection = dataBase.getCollection("test");
+		
+		BasicDBObject searchingObject = this.createObjectById(id);
+		
+		DBObject doc = new BasicDBObject();
+		doc.put("name", "test2");
+		collection.update(
+				searchingObject,
+				new BasicDBObject("$set",doc)
+				);
+		
+		return collection.findOne(searchingObject).toString();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public String removeElement(@PathParam("id") String id)
+	{
+		try{
+			DB dataBase = mongo.getDB("test");
+			DBCollection collection = dataBase.getCollection("test");
+			collection.remove(createObjectById(id));	
+			return "{\"message\":\"remove is ok\"}";
+		}catch(Exception e){
+			return "{\"error\":\""+e.getMessage()+"\"}";
+		}
+	}
+	
+	private BasicDBObject createObjectById(String id)
+	{
+		return new BasicDBObject("_id",new ObjectId(id));
+	}
+	
 }
